d7bc153e03b69691f884c5f06742ea6f6f68e73c
KaedinKurtz <Kaedinkay03@gmail.com>
2025-06-08 13:52:31 -0400

    Added More GridComponent Attributes

--- include/components.hpp
+++ include/components.hpp
@@ -32,12 +32,37 @@ struct GridComponent
 {
     // --- ADD THIS LINE ---
     bool visible = true;
+    bool visible2 = true;
+    bool visible3 = true;
+    bool visible4 = true;
+    bool visible5 = true;
 
     std::vector<GridLevel> levels;
     float baseLineWidthPixels = 1.0f;
+
     bool showAxes = true;
+    bool isMetric = true;
+
     glm::vec3 xAxisColor = { 1.0f, 0.2f, 0.2f };
     glm::vec3 zAxisColor = { 0.2f, 0.2f, 1.0f };
+
+    glm::vec3 xAxis2Color = { 1.0f, 0.2f, 0.2f };
+    glm::vec3 zAxis2Color = { 0.2f, 0.2f, 1.0f };
+
+    glm::vec3 xAxis3Color = { 1.0f, 0.2f, 0.2f };
+    glm::vec3 zAxis3Color = { 0.2f, 0.2f, 1.0f };
+
+    glm::vec3 xAxis4Color = { 1.0f, 0.2f, 0.2f };
+    glm::vec3 zAxis4Color = { 0.2f, 0.2f, 1.0f };
+
+    glm::vec3 xAxis5Color = { 1.0f, 0.2f, 0.2f };
+    glm::vec3 zAxis5Color = { 0.2f, 0.2f, 1.0f };
+
+    glm::vec3 origin = { 1.0f, 0.2f, 0.2f };
+
+    glm::vec3 eulerOrient = { 1.0f, 0.2f, 0.2f };
+	glm::vec4 quaternionOrient = { 1.0f, 0.2f, 0.2f, 0.3f };
+
     float axisLineWidthPixels = 1.4f;
 };
 
