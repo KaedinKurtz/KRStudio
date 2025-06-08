2fa81d274fec8774c0d61c965ec3618eefe0ae61
KaedinKurtz <Kaedinkay03@gmail.com>
2025-06-08 14:59:17 -0400

    Designed Grid Properties Customization UI

---
+++ src/gridPropertiesWidget.cpp
@@ -0,0 +1,11 @@
+#include "gridPropertiesWidget.h"
+
+gridPropertiesWidget::gridPropertiesWidget(QWidget *parent)
+	: QWidget(parent)
+{
+	ui.setupUi(this);
+}
+
+gridPropertiesWidget::~gridPropertiesWidget()
+{}
+
