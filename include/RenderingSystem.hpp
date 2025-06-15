a5c1d89b270842cb11c44384ce194c7292d6261e
Kaedin Kurtz <128757515+KaedinKurtz@users.noreply.github.com>
2025-06-15 00:21:03 -0400

    Add centralized RenderingSystem

---
+++ include/RenderingSystem.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Scene.hpp"
+#include "Shader.hpp"
+#include "Mesh.hpp"
+#include "components.hpp"
+#include <QOpenGLFunctions_3_3_Core>
+#include <glm/glm.hpp>
+
+namespace RenderingSystem
+{
+    void initialize(QOpenGLFunctions_3_3_Core* gl);
+    void shutdown(Scene* scene);
+    void uploadMeshes(Scene* scene);
+    void render(Scene* scene,
+                const glm::mat4& viewMatrix,
+                const glm::mat4& projectionMatrix,
+                const glm::vec3& cameraPos);
+}
